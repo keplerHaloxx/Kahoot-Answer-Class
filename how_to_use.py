f34037865a67e896793a447c50ee8ac2f23e9654
Haloxx <80098945+xvhHaloxx@users.noreply.github.com>
2022-12-06 17:33:56 +1100

    Update how_to_use.py

--- how_to_use.py
+++ how_to_use.py
@@ -1,11 +1,13 @@
 from KahootClass import Kahoot
 
 # 3fd3016c-fd92-440a-a614-5d5c6eb1c17f
-# This is the uuid of the kahoot (look at the host's screen and see the link
-# At the end there will be a part that says 'quizId=(where quiz id is)'
-# put that as the param for the Kahoot class
+# This is the uuid of a random Kahoot I found.
+# To see the quizId, look at the host's screen and see the link.
+# At the end there will be a part that says 'quizId=(where quizId is)'.
+# Put that as the param for the Kahoot class.
 
-kahoot = Kahoot("3fd3016c-fd92-440a-a614-5d5c6eb1c17f")
+# Or you can just do what I did and just prompt asking for the quizID.
+kahoot = Kahoot(input("Enter QuizID: "))
 
 for i in range(kahoot.get_quiz_length()):
     if kahoot.get_answer(i) is not None:
